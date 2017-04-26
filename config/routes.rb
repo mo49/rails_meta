f13f2a791dc033ca4b24e1474a6aaac05a5703af
mochizuki-kazuki <mochizuki-kazuki@kayac.com>
2017-04-26 13:58:03 +0900

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/' => 'site#index', as: "site_index"
+  get '/about' => 'site#about', as: "site_about"
+  get '/contact' => 'site#contact', as: "site_contact"
+
 end
